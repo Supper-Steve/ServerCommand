e5eed1ad5a69397146757f6f9b421f4b01cb56e0
史蒂夫 <steam114514@126.com>
2024-12-15 15:42:28 +0800

    源码

--- ServerCommand/Plugin.cs
+++ ServerCommand/Plugin.cs
@@ -7,7 +7,12 @@ namespace ServerCommand
     {
         [PluginConfig]
         public static Config Config;
-        [PluginEntryPoint("ServerCommand","2.1","ServerCommand","史蒂夫")]
-        public void Load() => Log.Info("加载服务器指令插件!");
+        [PluginEntryPoint("ServerCommand", "2.1", "ServerCommand", "史蒂夫")]
+        public void Load() 
+        {
+            Log.Info("加载服务器指令插件! By 史蒂夫");
+            Log.Info("如果你在某鱼或者其他平台上买到了这个插件,请立刻退款并举报");
+            Log.Info("https://github.com/Supper-Steve/InfAmmo");
+        }
     }
 }
